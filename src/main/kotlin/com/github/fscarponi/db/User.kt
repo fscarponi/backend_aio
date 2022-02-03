1f1e6c5e9cea9aea9ad6ced334d83fb8f98793f8
fscarponi <fscarponi@gmail.com>
2022-02-03 12:13:59 +0100

    verification token handling improved

--- src/main/kotlin/com/github/fscarponi/db/User.kt
+++ src/main/kotlin/com/github/fscarponi/db/User.kt
@@ -2,9 +2,11 @@ package com.github.fscarponi.db
 
 import kotlinx.serialization.Serializable
 import java.time.Instant
+import java.util.*
 
 @Serializable
 data class User(
+    val id: String = UUID.randomUUID().toString(),
     val mail: String,
     val hashPassword: String,
     @Serializable(with = InstantSerializer::class)
@@ -12,7 +14,8 @@ data class User(
     @Serializable(with = InstantSerializer::class)
     val lastPasswordModificationInstant: Instant? = null,
     val isBanned: Boolean = false,
-    val verificationToken: String
+    val verificationToken: String?,
+    val isVerified: Boolean = false
 )
 
 
--- src/main/kotlin/com/github/fscarponi/routes/auths/RegisterApi.kt
+++ src/main/kotlin/com/github/fscarponi/routes/auths/RegisterApi.kt
@@ -3,8 +3,10 @@ package com.github.fscarponi.routes.auths
 import com.github.fscarponi.db.User
 import com.github.fscarponi.db.databaseTransaction
 import com.github.fscarponi.routes.instance
+import com.mongodb.client.model.Filters
 import io.ktor.application.*
 import io.ktor.http.*
+import io.ktor.locations.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
@@ -35,10 +37,27 @@ fun Route.signupApi() = route("signup") {
         }
     }
 
-    //TODO ACTIVATION ROUTE OR LOCATION
+    get<VerifyRegistrationTokenLocation> { (emailBase64, token) ->
+        val mail = Base64.getDecoder().decode(emailBase64).toString()
+        val filter = Filters.and(
+            User::mail eq mail,
+            User::verificationToken eq token
+        )
+        databaseTransaction {
+            val user = usersCollection.findOne(filter)
+            user?.let {
+                usersCollection.replaceOneById(user.id, user.copy(isVerified = true, verificationToken = null))
+                call.respond(HttpStatusCode.OK)
+            } ?: call.respond(HttpStatusCode.BadRequest)
+        }
 
+
+    }
 
 }
+
+@Location("verify/{emailBase64}/{token}")
+data class VerifyRegistrationTokenLocation(val emailBase64: String, val token: String)
 
 @Serializable
 class UserRegistrationDataRequest(
